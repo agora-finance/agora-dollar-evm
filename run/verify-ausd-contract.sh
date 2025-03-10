f1480d901e3f3223ce45fdcc6ef145e77cbeb3b7
dirmes <jalonso7@fordham.edu>
2025-03-10 17:47:56 +0530

    op::testnet::Ausd

--- run/verify-ausd-contract.sh
+++ run/verify-ausd-contract.sh
@@ -5,18 +5,18 @@ set -euo pipefail
 source .env
 source run/print-highlighted.sh
 export FOUNDRY_PROFILE=deploy
-# export RPC_URL="https://rpc-amoy.polygon.technology/"
 # export RPC_URL="https://testnet-rpc2.monad.xyz/52227f026fa8fac9e2014c58fbf5643369b3bfc6"
-# export RPC_URL="https://ava-testnet.public.blastapi.io/ext/bc/C/rpc"
-# export RPC_URL="https://sepolia-rollup.arbitrum.io/rpc"
 # export EXPLORER_API_KEY="PAA4H13U39N9KPY2YES3UXRBUZ6NY84MS9" # ETH
+# export RPC_URL="https://ava-testnet.public.blastapi.io/ext/bc/C/rpc"
 # export EXPLORER_API_KEY="DZUJIDPM6NSEXVMD719UHEN3P5YPYV8NX3" # AVAX
+# export RPC_URL="https://sepolia-rollup.arbitrum.io/rpc"
 # export EXPLORER_API_KEY="DWK15KDFDXVQBYRSV5HWKG76TVFXDHDIBR" # ARB
+# export RPC_URL="https://rpc-amoy.polygon.technology/"
 # export EXPLORER_API_KEY="XFFIN7ITQGYEN74PU8RQSPARKGBHB8E8N7" # POS
-# export RPC_URL="https://sepolia.optimism.io"
+# export RPC_URL="https://optimism-sepolia.drpc.org"
 # export EXPLORER_API_KEY="I124XHV49G1JWY4ABNQBIHW4WX13S6Z33S" # OP
-export RPC_URL="https://sepolia.base.org"
-export EXPLORER_API_KEY="N9SVE6ZWKBAKZUN56NZ9WVQB6Q4S4YTN62"
+# export RPC_URL="https://sepolia.base.org"
+# export EXPLORER_API_KEY="N9SVE6ZWKBAKZUN56NZ9WVQB6Q4S4YTN62"
 
 JSON_FILE="run/contracts.json"
 
